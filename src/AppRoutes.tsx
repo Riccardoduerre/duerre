5935bcbd117dfa8b76cadd4148619a848c2de70f
Riccardoduerre <riccardo@duerremedia.com>
2026-07-29 13:19:55 +0000

    Fix locale route parameter to render localized pages

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -11,15 +11,7 @@ export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/en" replace />} />
-      <Route path="/en" element={<LocaleLayout />}>
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="portfolio" element={<Portfolio />} />
-        <Route path="blog" element={<Blog />} />
-        <Route path="blog/:slug" element={<BlogPost />} />
-        <Route path="contact" element={<Contact />} />
-      </Route>
-      <Route path="/it" element={<LocaleLayout />}>
+      <Route path="/:locale" element={<LocaleLayout />}>
         <Route index element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="portfolio" element={<Portfolio />} />
